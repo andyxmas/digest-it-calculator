e459782546f3aa0622e817cdd4032e11fe3031a3
andyxmas <andy.christmas@gmail.com>
2020-10-17 22:04:28 +0100

    standard tank calculator

--- pages/index.vue
+++ pages/index.vue
@@ -1,65 +1,119 @@
 <template>
-<div class="container p-2">
+  <div class="container p-2">
+    <h1 class="text-xl mb-8">Standard Tank Slurry Calculator</h1>
 
-  <h1 class="text-xl mb-8">How much digest-it do I need?</h1>
-
-  <div class="flex space-x-2">
-    <div>
-      <label for="cows" class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
-        Cows 
-      </label>
-      <input type="number" 
-        inputmode="numeric"
-        name="cows"
-        v-model="cows"
-        class="bg-gray-200 appearance-none border-2 border-gray-200 rounded py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500 w-20"
-      />
-    </div>
-    <div>
-      <label for="calves" class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
-        Calves 
-      </label>
-      <input type="number" 
-        inputmode="numeric"
-        name="calves"
-        v-model="calves"
-        class="bg-gray-200 appearance-none border-2 border-gray-200 rounded py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500 w-20"
+    <div class="flex space-x-2">
+      <div>
+        <label
+          for="length"
+          class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+        >
+          Length
+        </label>
+        <input
+          type="number"
+          inputmode="numeric"
+          name="length"
+          v-model="length"
+          class="bg-gray-200 appearance-none border-2 border-gray-200 rounded py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500 w-20"
         />
-    </div>
+      </div>
+      <div>
+        <label
+          for="width"
+          class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+        >
+          Width
+        </label>
+        <input
+          type="number"
+          inputmode="numeric"
+          name="width"
+          v-model="width"
+          class="bg-gray-200 appearance-none border-2 border-gray-200 rounded py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500 w-20"
+        />
+      </div>
+      <div>
+        <label
+          for="height"
+          class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+        >
+          Height
+        </label>
+        <input
+          type="number"
+          inputmode="numeric"
+          name="height"
+          v-model="height"
+          class="bg-gray-200 appearance-none border-2 border-gray-200 rounded py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500 w-20"
+        />
+      </div>
 
     <div>
-      <label for="required" class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
-          Digest-it required
-      </label>
-      <input type="text"
-      readonly 
-        name="required"
-        v-model="required"
-        class="bg-blue-200 appearance-none border-2 border-blue-200 rounded py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500 w-full"
-        />
+
+      <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="unit">Unit</label>
+      <div class="w-full">
+        <input type="radio" value="ft" name="unit" v-model="unit" />
+        <label for="ft">Feet</label>
+      </div>
+
+      <div class="w-full">
+        <input type="radio" value="m" name="unit" v-model="unit" />
+        <label for="m">Meters</label>
+      </div>
+
     </div>
-  </div>
+    
+    </div>
+
+<h2 class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4 mt-8">Slurry volume</h2>
+<div class="flex space-x-2">
+  
+
+    <p class="w-full"><strong>g</strong>: {{ g | dp }}</p>
+    <p class="w-full"><strong>M3</strong>: {{ m3 | dp }}</p>
+
 </div>
+
+<h2 class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4 mt-8">Digest-it required</h2>
+<p class="text-xl">{{ required}} </p>
+  </div>
 </template>
 
 <script>
 export default {
-  data: function() {
+  data: function () {
     return {
-      cows: 0,
-      calves: 0,
-      cows_multiplier: 0.1,
-      calves_multiplier: 0.05
+      length: 45,
+      width: 45,
+      height: 8,
+      unit: 'ft',
+      per_m_divisor: 22.73,
+      ft_to_m: 0.3048
     }
   },
   computed: {
-    required: function() {
-      let amount = this.cows * this.cows_multiplier + this.calves * this.calves_multiplier
-      return Math.ceil(amount)
-    }
+    g: function() {
+      if (this.unit == 'm') {
+        return (this.length * this.width * this.height) * 219.969
+      } else if (this.unit == 'ft') {
+        return ( (this.length * this.ft_to_m) * (this.width * this.ft_to_m)  * (this.height * this.ft_to_m) ) * 219.969
+      }
+    },
+    m3: function () {
+      return this.g/1000 * 4.5
+    },
+    required: function () {
+      let amount = this.m3 / this.per_m_divisor
+      return amount.toFixed(1)
+    },
+  },
+  filters: {
+  dp: function (number) {
+    return number.toFixed(0)
   }
+}
 }
 </script>
 
-<style>
-</style>
+<style></style>
